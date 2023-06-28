35e9b2ea5c0bbc0a82f841ea44c47d0328d00e64
Matthieu Mota <matthieumota@gmail.com>
2023-06-28 09:32:51 +0200

    Exo 2 boucles

---
+++ 03-boucles/exercice2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Les sushis</h1>
+    <?php
+        for ($i = 0; $i < 10; $i++) {
+            for ($j = 0; $j < $i + 1; $j++) {
+                echo '🍣 ';
+            }
+            echo '<br>';
+        }
+    ?>
+</body>
+</html>
