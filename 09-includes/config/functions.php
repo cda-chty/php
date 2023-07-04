13e9743efa7596a8f4bed90ea144b254659c0fd9
Matthieu Mota <matthieumota@gmail.com>
2023-07-04 12:32:32 +0200

    Validate form

--- 09-includes/config/functions.php
+++ 09-includes/config/functions.php
@@ -10,3 +10,32 @@ function pageName() {
 
     return $name;
 }
+
+/**
+ * Permet de valider un email.
+ */
+function validEmail(string $email): bool {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+/**
+ * Permet de valider des valeurs dans un tableau.
+ * ['html', 'toto'] => Pas valide
+ * ['html', 'php'] => Valide
+ */
+function validArray(array $data, array $valid): bool {
+    foreach ($data as $item) {
+        if (!in_array($item, $valid)) {
+            return false;
+        } 
+    }
+
+    return true;
+}
+
+/**
+ * Permet d'afficher checked si une valeur est dans un tableau.
+ */
+function checked(string $value, array $array): string {
+    return in_array($value, $array) ? 'checked' : '';
+}
