511ef6bde7c9dd5b50868d8dcc98fa93dafe6041
Matthieu Mota <matthieumota@gmail.com>
2023-07-05 16:00:23 +0200

    Header and footer

--- 10-pdo-mysql/config/functions.php
+++ 10-pdo-mysql/config/functions.php
@@ -30,3 +30,14 @@ function convertCsvToArray(string $file): array {
 
     return $data;
 }
+
+/**
+ * Renvoie le nom de la page actuelle.
+ */
+function pageName() {
+    $uri = $_SERVER['REQUEST_URI']; // /php/09-includes/contact.php
+    $name = strrchr($uri, '/'); // /contact.php
+    $name = substr($name, 1, -4); // contact
+
+    return $name;
+}
---
+++ 10-pdo-mysql/partials/header.php
@@ -0,0 +1,27 @@
+<?php
+require __DIR__.'/../config/functions.php';
+$title = isset($title) ? "$title - Films" : 'Films';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Films</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">Films</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link <?= pageName() === 'index' ? 'active' : ''; ?>" aria-current="page" href="index.php">Accueil</a>
+                    <a class="nav-link <?= pageName() === 'contact' ? 'active' : ''; ?>" href="contact.php">Contact</a>
+                </div>
+            </div>
+        </div>
+    </nav>
