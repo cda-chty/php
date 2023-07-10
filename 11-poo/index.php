80db8dc04c96d8c8c387978f2602a8af6a23a12e
Matthieu Mota <matthieumota@gmail.com>
2023-07-10 15:15:54 +0200

    Fix doctype

--- 11-poo/index.php
+++ 11-poo/index.php
@@ -5,7 +5,7 @@ require 'vendor/autoload.php';
 use App\Cat;
 
 $bianca = new Cat('Bianca');
-$bianca->setFur('blanc')->setFur('noir');
+$bianca->setFur('noir')->setFur('blanc');
 $mina = new Cat('Mina');
 $mina->setFur('noir');
 
@@ -13,12 +13,21 @@ dump($bianca, $mina);
 
 ?>
 
-<h1>Mon premier chat s'appelle <?= $bianca->getName(); ?></h1>
-<p>Et <?= $bianca->cry(); ?></p>
-<p>
-    L'autre chat est <?= $mina->getName(); ?>.
-    Et <?= $mina->cry(); ?>
-</p>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POO</title>
+</head>
+<body>
+    <h1>Mon premier chat s'appelle <?= $bianca->getName(); ?></h1>
+    <p>Et <?= $bianca->cry(); ?></p>
+    <p>
+        L'autre chat est <?= $mina->getName(); ?>.
+        Et <?= $mina->cry(); ?>
+    </p>
 
-<p><?= $bianca->playWith($mina); ?></p>
-
+    <p><?= $bianca->playWith($mina); ?></p>
+</body>
+</html>
