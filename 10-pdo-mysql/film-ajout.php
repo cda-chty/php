4385cc0c483f326b96042f702bfc9c35abdd1912
Matthieu Mota <matthieumota@gmail.com>
2023-07-10 11:22:21 +0200

    Upload

--- 10-pdo-mysql/film-ajout.php
+++ 10-pdo-mysql/film-ajout.php
@@ -9,7 +9,7 @@ $titleMovie = sanitize($_POST['title'] ?? null);
 $released_at = $_POST['released_at'] ?? null;
 $description = sanitize($_POST['description'] ?? null);
 $duration = $_POST['duration'] ?? null;
-// $cover = $_FILES['cover'] ?? null;
+$cover = $_FILES['cover'] ?? null;
 $category = $_POST['category'] ?? null;
 
 $errors = [];
@@ -36,7 +36,23 @@ if (submitted()) {
         $errors['duration'] = 'La durée doit être entre 1 et 999 minutes.';
     }
 
-    // @todo upload vérif
+    if ($cover['error'] !== 0) {
+        $errors['cover'] = "L'image est obligatoire";
+    } else {
+        // Type du fichier
+        $mime = mime_content_type($cover['tmp_name']); // image/jpeg image/png image/gif
+        $validMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
+
+        if (!in_array($mime, $validMimeTypes)) {
+            $errors['cover'] = 'Vous devez uploader une image.';
+        }
+
+        // 35674 = 35ko = 35674 / 1024
+        // 3 * 1024 * 1024 => on limite à 3Mo
+        if ($cover['size'] > 3 * 1024 ** 2) {
+            $errors['cover'] = "L'image doit faire 3Mo maximum.";
+        }
+    }
 
     $exists = db()->query('SELECT COUNT(id) FROM category WHERE id = '.intval($category))->fetchColumn();
     if (!$exists) {
@@ -45,8 +61,12 @@ if (submitted()) {
 
     if (empty($errors)) {
         // @todo upload
+        // $titleMovie = 'Le Parrain'; // le-parrain-1234
+        $extension = strrchr($cover['name'], '.'); // cat.jpg => .jpg
+        $filename = strtolower(str_replace(' ', '-', $titleMovie)).'-'.uniqid().$extension; // le-parrain-1234.jpg
+        move_uploaded_file($cover['tmp_name'], __DIR__.'/uploads/'.$filename);
 
-        $cover = 'le-parrain.jpg'; // Juste pour tester...
+        $cover = $filename; // Juste pour tester...
 
         // On fait la requête SQL pour insérer le film
         $query = db()->prepare('INSERT INTO movie (title, released_at, description, duration, cover, id_category)
@@ -87,7 +107,7 @@ require 'partials/header.php'; ?>
                 </div>
             <?php } ?>
 
-            <form method="post">
+            <form method="post" enctype="multipart/form-data">
                 <div class="mb-3">
                     <label for="title">Titre</label>
                     <input type="text" name="title" id="title" class="form-control" value="<?= $titleMovie; ?>">
@@ -110,7 +130,7 @@ require 'partials/header.php'; ?>
 
                 <div class="mb-3">
                     <label for="cover">Cover</label>
-                    <input type="text" name="cover" id="cover" class="form-control">
+                    <input type="file" name="cover" id="cover" class="form-control">
                 </div>
 
                 <div class="mb-3">
