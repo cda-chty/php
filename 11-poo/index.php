d30a27cb063d57856e509ef5aa6928f79202874e
Matthieu Mota <matthieumota@gmail.com>
2023-07-10 15:03:22 +0200

    First class

--- 11-poo/index.php
+++ 11-poo/index.php
@@ -4,5 +4,21 @@ require 'vendor/autoload.php';
 
 use App\Cat;
 
-$bianca = new Cat();
-var_dump($bianca);
+$bianca = new Cat('Bianca');
+$bianca->setFur('blanc')->setFur('noir');
+$mina = new Cat('Mina');
+$mina->setFur('noir');
+
+dump($bianca, $mina);
+
+?>
+
+<h1>Mon premier chat s'appelle <?= $bianca->getName(); ?></h1>
+<p>Et <?= $bianca->cry(); ?></p>
+<p>
+    L'autre chat est <?= $mina->getName(); ?>.
+    Et <?= $mina->cry(); ?>
+</p>
+
+<p><?= $bianca->playWith($mina); ?></p>
+
--- 11-poo/src/Cat.php
+++ 11-poo/src/Cat.php
@@ -4,4 +4,35 @@ namespace App;
 
 class Cat
 {
+    private string $name;
+    private string $type = 'Chat de gouttière';
+    private string $fur;
+
+    public function __construct(string $name, string $type = 'Chat de gouttière')
+    {
+        $this->name = $name;
+        $this->type = $type;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setFur(string $fur): self
+    {
+        $this->fur = $fur;
+
+        return $this;
+    }
+
+    public function cry(): string
+    {
+        return $this->name.' miaule';
+    }
+
+    public function playWith(Cat $otherCat): string
+    {
+        return $this->name.' joue avec '.$otherCat->name;
+    }
 }
